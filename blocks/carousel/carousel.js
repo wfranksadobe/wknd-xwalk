817c35eaa1c42f4b49f1a520315de34701f6acac
Dirk <drudolph@adobe.com>
2024-02-27 08:45:31 +0100

    fix: prevent carousel-buttons being detectd as block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,6 +1,5 @@
 export default function decorate(block) {
   const buttons = document.createElement('div');
-  buttons.className = 'carousel-buttons';
   [...block.children].forEach((row, i) => {
     const classes = ['image', 'text'];
     classes.forEach((e, j) => {
