bd24ee434eccd3725a57083dbde90611b8b802e0
Dirk <drudolph@adobe.com>
2024-03-15 16:28:44 +0100

    Revert "fix: update ui after modification"
    This reverts commit 91e1f3e2e429870710d31d64e43ab67f97f68f70.

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -39,7 +39,7 @@ async function applyChanges(event) {
       newMain.style.display = null;
       // eslint-disable-next-line no-use-before-define
       attachEventListners(newMain);
-      return newMain;
+      return true;
     }
 
     const block = element.parentElement?.closest('.block[data-aue-resource]') || element?.closest('.block[data-aue-resource]');
@@ -56,7 +56,7 @@ async function applyChanges(event) {
         await loadBlock(newBlock);
         block.remove();
         newBlock.style.display = null;
-        return newBlock;
+        return true;
       }
     } else {
       // sections and default content, may be multiple in the case of richtext
@@ -81,12 +81,12 @@ async function applyChanges(event) {
           decorateIcons(parentElement);
           decorateRichtext(parentElement);
         }
-        return newElements;
+        return true;
       }
     }
   }
 
-  return null;
+  return false;
 }
 
 function updateUi(event) {
@@ -118,12 +118,7 @@ function attachEventListners(main) {
   ].forEach((eventType) => main?.addEventListener(eventType, async (event) => {
     event.stopPropagation();
     const applied = await applyChanges(event);
-    if (!applied) {
-      window.location.reload();
-      return;
-    }
-
-    updateUi({ detail: { selected: true }, target: applied });
+    if (!applied) window.location.reload();
   }));
 
   main.addEventListener('aue:ui-select', updateUi);
