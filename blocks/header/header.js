95a571a6cad511941e421f13eb16a15de03ad857
William Franks <136557841+wfranksadobe@users.noreply.github.com>
2025-05-12 19:25:41 +1000

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -21,7 +21,7 @@ export default async function decorate(block) {
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : (window.wknd.demoConfig.demoBase || '/nav');
 
-  const resp = await fetch(`${getSiteRoot(5)}${navPath}.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
+  const resp = await fetch(`${getSiteRoot()}${navPath}.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
   if (resp.ok) {
     const html = await resp.text();
 
